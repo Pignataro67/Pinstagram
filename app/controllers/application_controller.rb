dd3b62eec7137a9b5aa113f8ed3d78e2569a9065
Pignataro67 <Pignataro67@gmail.com>
2020-11-19 05:13:39 -0800

    user setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
     set :views, 'app/views'
   enable :sessions
     set :session_secret, 'finstagram_mek'
+    use Rack::Flash
   get "/" do
     erb :welcome	
-    "welcome to finstagram"
-  end    
+    "welcome to pinstagram"
+    helpers do
+      def current_user
+        User.find(session[:user_id])
+      end
+  
+      def is_logged_in?(session)
+        session.has_key?(:user_id)
+      end
+    end  
 end
