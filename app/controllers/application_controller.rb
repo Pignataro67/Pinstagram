745d65f00c6c2756fd9ddab6f6a46e0e478ee6d0
Pignataro67 <Pignataro67@gmail.com>
2020-11-19 05:11:09 -0800

    mo' setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  enable :sessions
+    set :session_secret, 'finstagram_mek'
   get "/" do
     erb :welcome	
     "welcome to finstagram"
