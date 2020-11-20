e11f6bca2c63394a007d2462694f35f9903311e8
Pignataro67 <Pignataro67@gmail.com>
2020-11-20 06:23:27 -0800

    improving user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
   validates :username, :password_digest, presence: true
+  def self.find_by_slug(slug)
+    self.all.find {|occurence| occurence.slug == slug}
+  end
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
 end
