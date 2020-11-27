1284252733242dd60733d8cacc769c3088b23c86
Pignataro67 <Pignataro67@gmail.com>
2020-11-27 07:23:06 -0800

    cleanup/refractor

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,35 +1,5 @@
 class UserController < ApplicationController
 
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'/users/users'
-  end
-
-  post '/users/:id/edit' do
-    @user = User.find_by_id(params[:id])
-    if is_logged_in?(session) && @user.id == current_user.id
-      erb :'/users/edit'
-    elsif is_logged_in?(session) && @user.id != current_user.id
-    flash[:error] = "This is not your profile to edit"
-    redirect '/posts'
-    else
-      flash[:error] = "Please log in"
-      redirect '/login'
-    end
-  end
-  
-  patch '/users/:id' do
-    user = User.find_by_id(params[:id])
-    if params[:user_photo] != ""
-      user.update(user_photo: params[:user_photo])
-      flash[:success] = "Profile successfully updated!"
-      redirect "/users/#{user.id}"
-    else
-      flash[:error] = "Your profile did not update correctly"
-      redirect "/users/#{user.id}/edit"
-    end
-  end
-
   get '/login' do
     if !is_logged_in?(session)
       erb :'/users/login'
