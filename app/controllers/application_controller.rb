fde39fe402f6242584ffd33f45af85e0c55f69c1
Pignataro67 <Pignataro67@gmail.com>
2020-12-08 06:16:57 -0800

    spacing, refractoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,23 @@
 class ApplicationController < ActionController::Base
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  enable :sessions
+    enable :sessions
     set :session_secret, 'finstagram_mek'
     use Rack::Flash
-  get "/" do
-    erb :welcome	
-    "welcome to pinstagram"
-    helpers do
-      def current_user
-        User.find(session[:user_id])
-      end
+  end
+
+  get '/' do
+    erb :index	
+  end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
   
-      def is_logged_in?(session)
-        session.has_key?(:user_id)
-      end
-    end  
+    def is_logged_in?!!current_user
+    end
+  end  
 end
